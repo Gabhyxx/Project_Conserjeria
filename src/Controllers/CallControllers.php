97c3a1e0507e57e75ed07b8dead0ceed01c97398
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-11-11 15:51:59 +0100

    feat: Callcontrollers

---
+++ src/Controllers/CallControllers.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\controllers
+use App\Models\Call;
+class controller{
+
+
+public function __construct(){
+    $this->index();
+}
+public function index(){
+    $call new = call;
+    }
+
+}
