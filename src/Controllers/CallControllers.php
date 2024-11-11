848ca42b46fa0b4a95a1ee9ab108703e69946e5c
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-11-11 17:32:32 +0100

    refactor: callcontrollers

--- src/Controllers/CallControllers.php
+++ src/Controllers/CallControllers.php
@@ -1,15 +1,17 @@
 <?php
 
-namespace App\controllers
-use App\Models\Call;
-class controller{
+namespace App\Controllers;
 
+use App\Models\Call;
+
+class Controller{
 
 public function __construct(){
     $this->index();
 }
 public function index(){
-    $call new = call;
+    $call = new = call;
+    $calls = new = call;->all();
     }
 
 }
