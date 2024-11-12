4afbc83486e5dd610a5ddc35a827ee960e9739b7
ruzo0 <ishakchahboun6@gmail.com>
2024-11-12 18:19:46 +0100

    feat: add header

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión de Averías</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Hotel - Gestión de Averías</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="nueva_averia.php">Registrar Avería</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
