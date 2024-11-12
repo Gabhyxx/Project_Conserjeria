404ed959fdf1098e18a5b2aa22184783bcf2ffbf
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-11-12 13:47:24 +0100

    feat: lines 27  CallControlles

--- src/Controllers/CallControllers.php
+++ src/Controllers/CallControllers.php
@@ -7,12 +7,24 @@ use App\Core\View;
 class CallController{
 
 public function __construct(){
+
+    if (isset($_GET["action"]) && ($_GET["action"] =="delete"))
+{
+    $this->delete($_GET["id"]);
+}
+        return;
+
     $this->index();
 }
 public function index(){
+
     $call = new call;
     $calls = call->all();
     new view ("callList", ["call" => $calls]); }
     }
+    public function delete ($id){
+        $callDelete = new call;
+        $call =  $callDelete->
+    }
+}    
 
-}
