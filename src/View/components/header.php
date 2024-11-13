59be194a7b8a1cb565bb350a925f8148725aa6d8
ruzo0 <ishakchahboun6@gmail.com>
2024-11-13 13:20:49 +0100

    style: header style

--- src/View/components/header.php
+++ src/View/components/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="d-flex p-3 width=20px">
-        <img class="imgPersonHeader" src="https://w7.pngwing.com/pngs/416/25/png-transparent-concierge-hotel-computer-icons-apartment-hotel-blue-service-apartment-thumbnail.png">
+        <img class="imgPersonHeader" src="https://cdn-icons-png.freepik.com/512/6032/6032090.png?ga=GA1.1.271187742.1731067522">
         <h1 class="col-lg-6 px-4 text-black">Hotel Mercado Del Carmen ***</h1>
     </div>
 </header>
