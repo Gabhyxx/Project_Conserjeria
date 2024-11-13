9102adbcf967f9d4604716fc20a1833912d9cc66
TOKIO-V2 <albertohernandezgaleote6@gmail.com>
2024-11-13 11:31:47 +0100

    Feature/Added buttons

--- src/View/callList.php
+++ src/View/callList.php
@@ -7,32 +7,42 @@
          ?>
        <main>
        <table class="table">
-        <thead>
-            <tr>
-                <th>Date / Time</th>
-                <th>Room</th>
-                <th>Issues</th>
-                <th>Id</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                foreach ($data["call"] as $call) {
-                    echo "
-                            <tr>
-                                <td>{$call->dateTime}</td>
-                                <td>{$call->room}</td>
-                                <td>{$call->issue}</td>
-                                <td>{$call->id}</td>
-                
-                
-                            </tr>";
+            <thead>
+                <tr>
+                    <th>Date / Time</th>
+                    <th>Room</th>
+                    <th>Issues</th>
+                    <th>Id</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach ($data["call"] as $call) {
+                        echo "
+                                <tr>
+                                    <td>{$call->dateTime}</td>
+                                    <td>{$call->room}</td>
+                                    <td>{$call->issue}</td>
+                                    <td>{$call->id}</td>
+                                    <td>
+                                     <a href="?action=delete&id={$call->id}">❌</a></td>
+                    
+                    
+                                </tr>";
+ 
+
+                    }    
+                ?>
+            </tbody>
+        </table>
+
+        <div>
+            <a href="?action=create">
+                <button type="button" class="btn btn-success">Add problem</button>
+            </a>
+        </div>
 
 
-                }    
-            ?>
-        </tbody>
-</table>
        </main>
        <?php
             require_once("Components/footer.php")
